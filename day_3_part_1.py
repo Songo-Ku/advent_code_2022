c5638d14526d4255b2c0419a033e464152851c4c
arkadiusz <a.mosiejczuk.post@gmail.com>
2022-12-07 02:20:10 +0100

    added fix for part 1 divide in split character

--- day_3_part_1.py
+++ day_3_part_1.py
@@ -8,14 +8,13 @@ values_dict = dict(zip(string.ascii_lowercase + string.ascii_uppercase, range(1,
 calculation_sum = 0
 # iterate through all instructions to calculate sum
 for row in data_file.readlines():
-    extracted_1 = row[0:int(len(row) / 2)]
-    extracted_2 = row[int(len(row) / 2): len(row)]
+    extracted_1 = row[0:len(row) // 2]
+    extracted_2 = row[len(row) // 2: len(row)]
     # find common letter
     for letter in extracted_1:
         if letter in extracted_2:
             calculation_sum += values_dict[letter]
             break
 print(calculation_sum)
-
 
 
